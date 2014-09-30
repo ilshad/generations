fa0ddea4fda349ecb55395138daecd3380fcbfa6
Ilshad Khabibullin <astoon.net@gmail.com>
2014-09-30 21:20:00 +0400

    init commit

---
+++ test/ilshad/generations_test.clj
@@ -0,0 +1,7 @@
+(ns ilshad.generations-test
+  (:require [clojure.test :refer :all]
+            [ilshad.generations :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
